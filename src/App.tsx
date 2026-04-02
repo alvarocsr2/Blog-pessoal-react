3a5e3765b66ec951115e0c7d924412bff5c7615c
alvarocsr2 <alvarocesarmm@gmail.com>
2026-04-02 20:03:01 -0300

    ✨ New Feature: Construção dos Componentes Navbar e Footer. Adição das rotas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/home/Home"
+import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login" 
+import Navbar from "./assets/components/navbar/Navbar"
+import Footer from "./assets/components/footer/Footer"
 
 
 function App() {
-
+  
   return (
-    <>{/** Precisamos fazer uma div fantasma para agrupar os elementos filhos e não cria uma div na dom */}
-   <Home/>
-   </>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
   )
 }
 
